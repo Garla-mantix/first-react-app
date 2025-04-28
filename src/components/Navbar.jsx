a6b9bb980ff654489b761ff5a69e9370397bed12
Luca Pirro <aspudds-luca@hotmail.com>
2025-04-28 02:20:03 +0200

    navigation solved but ugly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <div>
-            <nav>
+            <nav className="z-50">
                 <ul>
                     <li> <Link to="/">Home</Link></li>
                     <li> <Link to="/about">About</Link></li>
