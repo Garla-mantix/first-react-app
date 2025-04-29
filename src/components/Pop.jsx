c1491c38a9b297c38f7109405885850fab8366bb
Luca Pirro <aspudds-luca@hotmail.com>
2025-04-29 17:13:20 +0200

    popup first edition

---
+++ src/components/Pop.jsx
@@ -0,0 +1,35 @@
+import { CheckIcon } from '@heroicons/react/20/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Pop({ popupInfo, onClose }) {
+  return (
+    <div className="fixed inset-0 bg-opacity-50 z-50 flex items-center justify-center">
+      <div
+        className={classNames(
+          'relative bg-gray-900 shadow-2xl',
+          'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10 max-w-lg w-full'
+        )}>
+        <button
+  onClick={() => {
+    onClose();
+  }}
+  className="absolute top-4 right-4 text-gray-400 hover:text-white text-3xl font-bold">
+  ×
+</button>
+        <p className="text-5xl font-semibold tracking-tight text-white">
+          {popupInfo.title}
+        </p>
+        <p className="mt-6 text-base/7 text-gray-300">{popupInfo.info}</p>
+        <a
+          href="/Projects"
+          className="mt-8 block rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 sm:mt-10"
+        >
+          See more
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/Portfolioproject.jsx
+++ src/components/Portfolioproject.jsx
@@ -2,23 +2,47 @@ import screenshot1 from "../assets/typing-game.png";
 import screenshot2 from "../assets/genesis-project.png";
 import screenshot3 from "../assets/bank.png";
 import '../Portfolioproject.css';
+import { useState } from "react";
+import Pop from "../components/Pop";
 
 function Portfolioproject(props) {
-
+    const [isOpen, setIsOpen] = useState(false);
+    const [popupInfo, setPopupInfo] = useState({ title: "", info: "" });
+  
+    const handleOpen = () => {
+      setPopupInfo({
+        title: props.name,
+        info: props.func,
+      });
+      setIsOpen(true);
+    };
+  
     return (
-        <div className="project">
-             <h2 className="project-title">{props.name}</h2>
-            <img src={props.screenshot} alt="a screenshot" className="project-screenshot" />
-            <a href={props.github} className="project-github">{props.github}</a>
-            <p className="project-tech">{props.tech}</p>
-            <ul className="project-info">
-                <li className="project-info__item">{props.func}</li>
-                <li className="project-info__item">{props.lesson}</li>
-                <li className="project-info__item">{props.role}</li>
-                <li className="project-info__item">{props.challenge}</li>
-            </ul>
+      <div className="project relative">
+        <div onClick={handleOpen} className="cursor-pointer">
+          <h2 className="project-title">{props.name}</h2>
+          <img src={props.screenshot} alt="a screenshot" className="project-screenshot" />
         </div>
+  
+        <a href={props.github} className="project-github">{props.github}</a>
+        <p className="project-tech">{props.tech}</p>
+        <ul className="project-info">
+          <li className="project-info__item">{props.func}</li>
+          <li className="project-info__item">{props.lesson}</li>
+          <li className="project-info__item">{props.role}</li>
+          <li className="project-info__item">{props.challenge}</li>
+        </ul>
+        {isOpen && (<Pop onClose={() => setIsOpen(false)} popupInfo={popupInfo} />)}
+      </div>
     );
-}
-
+  }
+  
 export default Portfolioproject;
+
+
+
+
+
+
+
+
